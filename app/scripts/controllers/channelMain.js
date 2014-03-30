b6c6d410b25395b55479c73cc8395aabf7a60b9c
Michael Morowitz <michael@morowitz.com>
2014-03-30 10:49:30 -0500

    added basic event input and display

--- app/scripts/controllers/channelMain.js
+++ app/scripts/controllers/channelMain.js
@@ -5,11 +5,25 @@ app.controller('ChannelController', function ($scope, $routeParams, Channel) {
 
 	$scope.addMessage = function () {
 		$scope.message.createdAt = new Date().getTime();
+		
+		//TODO: properly instatiate model object. This doesn't seem right.
 		if ($scope.channel.messages === undefined) {
 			$scope.channel.messages = [];
 		}
 		$scope.channel.messages.push($scope.message);
 		Channel.update($routeParams.channelId, $scope.channel);
+		
+		//TODO: Properly re-initialize UI? This doesn't seem right.
 		$scope.message.text = '';
 	};
+
+	$scope.addEvent = function () {
+		$scope.event.createdAt = new Date().getTime();
+		if ($scope.channel.events === undefined) {
+			$scope.channel.events = [];
+		}
+		$scope.channel.events.push($scope.event);
+		Channel.update($routeParams.channelId, $scope.channel);
+
+	}
 });
