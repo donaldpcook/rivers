fce2b7579c543fd84eda3d0d7801c1b0d7b2c1c2
Donald Cook <Donald.Cook@rga.com>
2014-04-07 12:40:32 -0500

    added login event notification

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -1,13 +1,28 @@
 'use strict';
 
-app.controller('AccountController', function ($scope, AccountService) {
-  $scope.login = function() {
-    AccountService.login($scope.user.email, $scope.user.password);
-  };
+app.controller('AccountController', function ($scope, $rootScope, $location, AccountService) {
+	$scope.login = function() {
+		AccountService.login($scope.userInput.email, $scope.userInput.password);
+	};
 
-  $scope.signup = function() {
-    AccountService.signup($scope.user.email, $scope.user.password).then(function(user) {
-      debugger;
-    });
-  };
+	$scope.signup = function() {
+		AccountService.signup($scope.user.email, $scope.user.password).then(function(user) {
+			$location.path('/');
+		});
+	};
+
+	$rootScope.$on('user:changed', function(evt, error, user) {
+		if (error) {
+			$scope.user = null;
+			$scope.error = error;
+		} else if (user) {
+			$scope.error = null;
+			$scope.user = user;
+		} else {
+			$scope.error = null;
+			$scope.user = null;
+		}
+
+		$scope.$apply();
+	});
 });
--- app/scripts/services/account.js
+++ app/scripts/services/account.js
@@ -1,36 +1,39 @@
 'use strict';
 
-app.factory('AccountService', function ($q, configuration) {
-	var ref = new Firebase(configuration.FIREBASE_URL + 'channels');
+app.factory('AccountService', function ($rootScope, $q, configuration) {
+	var ref = new Firebase(configuration.FIREBASE_URL);
+	var user;
 
-  // triggered anytime login state is changed
+	// triggered anytime login state is changed
 	var auth = new FirebaseSimpleLogin(ref, function(error, user) {
-  });
+		user = user;
+		$rootScope.$broadcast('user:changed', error, user);
+	});
 
-  var login = function(email, password) {
-    auth.login('password', {
-      email: email,
-      password: password
-    });
-  };
+	var login = function(email, password) {
+		auth.login('password', {
+			email: email,
+			password: password
+		});
+	};
 
-  var signup = function(email, password) {
-    var deferred = $q.defer();
+	var signup = function(email, password) {
+		var deferred = $q.defer();
 
-    auth.createUser(email, password, function(error, user) {
-      if (error) {
-        deferred.reject(error);
-      } else {
-        deferred.resolve(user);
-      }
-    });
+		auth.createUser(email, password, function(error, user) {
+			if (error) {
+				deferred.reject(error);
+			} else {
+				deferred.resolve(user);
+			}
+		});
 
-    return deferred.promise;
-  };
+		return deferred.promise;
+	};
 
 	var AccountService = {
-    login: login,
-    signup: signup
+		login: login,
+		signup: signup
 	};
 
 	return AccountService;
